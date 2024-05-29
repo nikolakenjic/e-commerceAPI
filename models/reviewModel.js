9b531eeaa61893cb5062fc917e12fb49db6ee094
Nikola Kenjic <nikola.nico89@gmail.com>
2024-05-29 20:07:34 +0200

    review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,4 +32,40 @@ const reviewSchema = new mongoose.Schema(
 // User review only once per product
 reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+reviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    {
+      $match: {
+        product: productId,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: '$rating' },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+  try {
+    await this.model('Product').findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+reviewSchema.post('save', async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+reviewSchema.post('remove', async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
 module.exports = mongoose.model('Review', reviewSchema);
